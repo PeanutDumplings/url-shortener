ba6a4f30cd94ed5198583fa18b450f4cf94cb9a0
PeanutDumplings <nicholas.whyte@outlook.com>
2024-07-28 17:22:50 +0930

    Large troubleshooting + finish backend functionality with typesafety

--- app/[id]/route.ts
+++ app/[id]/route.ts
@@ -1,11 +1,24 @@
 import redis from "@/lib/redis";
-import { NextRequest, NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const id = params.id;
-  const url = await redis.get(id);
-  return NextResponse.json({ t: url });
+  try {
+    const id = params.id;
+    const url = await redis.get(id);
+    if (!url || typeof url !== "string") {
+      return NextResponse.json(
+        { success: false, message: "Shortened URL not found" },
+        { status: 400, headers: { "content-type": "application/json" } }
+      );
+    }
+    return NextResponse.redirect(url, 302);
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: "Internal Server Error" },
+      { status: 500, headers: { "content-type": "application/json" } }
+    );
+  }
 }
